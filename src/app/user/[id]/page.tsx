8e7f41cd77b3a09cb8c4dbb5a0537619964282df
Mohamed AbdSabour <9abour@gmail.com>
2024-07-04 13:11:45 +0300

    RM-14 finish the creator profile (UI).

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,18 +1,11 @@
-import UserHeader from "@/components/creator-profile/components/UserHeader";
-import UserRoadmaps from "@/components/creator-profile/components/UserRoadmaps";
-import UserSideDetails from "@/components/creator-profile/components/UserSideDetails";
+import CreatorProfile from "@/components/creator-profile/components/CreatorProfile";
 import React from "react";
 
 const page = () => {
 	return (
-		<div className="relative max-w-[1440px] mx-auto p-4">
-			<UserHeader />
-
-			<div className="flex flex-col md:flex-row">
-				<UserSideDetails />
-				<UserRoadmaps />
-			</div>
-		</div>
+		<>
+			<CreatorProfile />
+		</>
 	);
 };
 
