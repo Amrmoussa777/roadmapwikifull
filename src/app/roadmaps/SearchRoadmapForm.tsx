7c7ff3a6d59184a2e0da29b8b38fc088698a4c50
Mohamed AbdSabour <9abour@gmail.com>
2024-07-29 19:21:01 +0300

    RM-74 implement roadmaps page header, search form.

--- src/app/roadmaps/SearchRoadmapForm.tsx
+++ src/app/roadmaps/SearchRoadmapForm.tsx
@@ -1,10 +1,17 @@
+"use client";
+
 import VerticalDivider from "@/components/common/divider/components/VerticalDivider";
-import { ROADMAP_ICON } from "@public/icons/roadmaps";
+import useToggle from "@/hooks/useToggle";
+import { ARROW_ICON } from "@public/icons/roadmapSteps";
+import { ROADMAP_ICON, SEARCH_ICON } from "@public/icons/roadmaps";
 import React from "react";
 
 const SearchRoadmapForm = () => {
+	const { currentState: isOptionsHidden, toggle: hideOptions } =
+		useToggle(false);
+
 	return (
-		<form className="w-full h-[80px] flex items-center mt-[20px] shadow-csm rounded-full py-[20px] px-[32px]">
+		<form className="w-full h-[80px] flex items-center mt-[20px] shadow-[0_8px_30px_rgb(0,0,0,0.12)] rounded-[16px] p-[20px] border border-transparent focus-within:border-primary-ultramarineBlue focus-within:shadow-[5px_5px_0px_0px_rgba(80,108,240),0_8px_30px_rgb(0,0,0,0.12)] transition duration-200">
 			<div className="flex gap-4 w-2/4">
 				<span className="text-[#C4C4C4]">{ROADMAP_ICON}</span>
 				<input
@@ -16,7 +23,27 @@ const SearchRoadmapForm = () => {
 
 			<VerticalDivider width="w-[1px]" bgColor="bg-[#EDEFF5]" />
 
-			<div></div>
+			<div className="w-2/4 flex-jb-c">
+				<button
+					id="roadmapDuration"
+					type="button"
+					onClick={hideOptions}
+					className="w-fit flex-jb-c roadmap-info-select font-poppins text-[16px] border-none sm:text-[18px] text-[#383838]"
+				>
+					Roadmaps
+					<span
+						className={`!text-[#3F3F3F] [&>svg]:transition-all ${
+							isOptionsHidden ? "[&>svg]:rotate-0" : "[&>svg]:rotate-180"
+						}`}
+					>
+						{ARROW_ICON}
+					</span>
+				</button>
+
+				<button className="w-[48px] h-[48px] flex-jc-c bg-[#F6F6F6] text-[#383838] rounded-full">
+					{SEARCH_ICON}
+				</button>
+			</div>
 		</form>
 	);
 };
--- src/app/roadmaps/page.tsx
+++ src/app/roadmaps/page.tsx
@@ -1,5 +1,6 @@
 import SearchRoadmapForm from "@/app/roadmaps/SearchRoadmapForm";
 import Header from "@/components/roadmaps/components/Header";
+import { ARROW_ICON } from "@public/icons/roadmapSteps";
 import React from "react";
 
 const page = () => {
@@ -8,6 +9,20 @@ const page = () => {
 			<Header />
 
 			<SearchRoadmapForm />
+
+			{/* Sorting */}
+			<div className="flex-jb-c mt-[32px]">
+				<h3 className="font-poppins text-[#9A9A9A] font-normal">
+					Result: <span className="font-medium text-[#484848]">40 roadmap</span>
+				</h3>
+				<div className="flex-jc-c font-poppins font-normal text-[14px]">
+					<p className="text-[#9A9A9A] mr-1">Sort by:</p>
+
+					<button className="w-fit flex-jc-c gap-1 font-poppins font-normal text-[14px] text-[#4E5D78] [&>svg]:w-[20px] [&>svg]:rotate-180">
+						Recently Added {ARROW_ICON}
+					</button>
+				</div>
+			</div>
 		</main>
 	);
 };
